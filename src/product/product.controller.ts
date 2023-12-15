e748f8c1076b4145189ff71b6676f837e6738e10
jose marchioni <jose.marchioni@alumnos.ucn.cl>
2023-12-14 23:18:33 -0300

    Product Complete

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -10,28 +10,29 @@ import {
   DeleteProductByIdRequest,
   DeleteProductByIdResponse,
   Empty,
+  PRODUCT_SERVICE_NAME
 } from './product.pb';
 
-@Controller('product')
+@Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @GrpcMethod('ProductService', 'addProduct')
+  @GrpcMethod(PRODUCT_SERVICE_NAME, 'addProduct')
   async addProduct(request: AddProductRequest): Promise<AddProductResponse> {
     return this.productService.addProduct(request);
   }
 
-  @GrpcMethod('ProductService', 'getProductById')
+  @GrpcMethod(PRODUCT_SERVICE_NAME, 'getProductById')
   async getProductById(request: getProductByIdRequest): Promise<getProductByIdResponse> {
     return this.productService.getProductById(request);
   }
 
-  @GrpcMethod('ProductService', 'getAllProducts')
+  @GrpcMethod(PRODUCT_SERVICE_NAME, 'getAllProducts')
   async getAllProducts(request: Empty): Promise<GetAllProductsResponse> {
     return this.productService.getAllProducts(request);
   }
 
-  @GrpcMethod('ProductService', 'deleteProductById')
+  @GrpcMethod(PRODUCT_SERVICE_NAME, 'deleteProductById')
   async deleteProductById(request: DeleteProductByIdRequest): Promise<DeleteProductByIdResponse> {
     return this.productService.deleteProductById(request);
   }
